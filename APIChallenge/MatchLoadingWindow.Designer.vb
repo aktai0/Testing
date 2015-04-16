2840e59d01398ac1b00d5129a009e382f55cc325
aktai0 <xtoken.x@gmail.com>
2015-04-16 08:20:54 -0700

    Added icon for match loading window

--- APIChallenge/MatchLoadingWindow.Designer.vb
+++ APIChallenge/MatchLoadingWindow.Designer.vb
@@ -22,6 +22,7 @@ Partial Class MatchLoadingWindow
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+      Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MatchLoadingWindow))
       Me.URFTimeLabel = New System.Windows.Forms.Label()
       Me.StartButton = New System.Windows.Forms.Button()
       Me.StopButton = New System.Windows.Forms.Button()
@@ -176,6 +177,7 @@ Partial Class MatchLoadingWindow
       Me.Controls.Add(Me.StopButton)
       Me.Controls.Add(Me.StartButton)
       Me.Controls.Add(Me.URFTimeLabel)
+      Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
       Me.Name = "MatchLoadingWindow"
       Me.Text = "Match Loading"
       Me.StatusStrip1.ResumeLayout(False)
